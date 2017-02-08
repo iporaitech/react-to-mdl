3e8cbbd32f2aa9847a3e4c769f9f6989312f39fc
hisa <hisa@iporaitech.com>
2017-02-08 16:34:44 -0300

    Change Button to functional component. Refs #11

--- src/button/index.js
+++ src/button/index.js
@@ -6,51 +6,51 @@
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 
-export default class Button extends React.Component {
-  static propTypes = {
-    className: PropTypes.string,
-    type: PropTypes.string,
-    raised: PropTypes.bool,
-    fab: PropTypes.bool,
-    miniFab: PropTypes.bool,
-    icon: PropTypes.bool,
-    colored: PropTypes.bool,
-    primary: PropTypes.bool,
-    accent: PropTypes.bool,
-    rippleEffect: PropTypes.bool
-  }
+const Button = (props) => {
+  const { className, raised, fab, miniFab, icon, colored,
+    primary, accent, rippleEffect, children, ...otherProps } = props;
 
-  static defaultProps = {
-    type: 'submit',
-    raised: false,
-    fab: false,
-    miniFab: false,
-    icon: false,
-    colored: false,
-    primary: false,
-    accent: false,
-    rippleEffect: true
-  }
+  const classes = classNames('mdl-button mdl-js-button', {
+    'mdl-button--raised': raised,
+    'mdl-button--fab': fab,
+    'mdl-button--mini-fab': miniFab,
+    'mdl-button--icon': icon,
+    'mdl-button--colored': colored,
+    'mdl-button--primary': primary,
+    'mdl-button--accent': accent,
+    'mdl-js-ripple-effect': rippleEffect
+  }, className);
 
-  render() {
-    const { className, raised, fab, miniFab, icon, colored,
-      primary, accent, rippleEffect, children, ...otherProps } = this.props;
-
-    const classes = classNames('mdl-button mdl-js-button', {
-      'mdl-button--raised': raised,
-      'mdl-button--fab': fab,
-      'mdl-button--mini-fab': miniFab,
-      'mdl-button--icon': icon,
-      'mdl-button--colored': colored,
-      'mdl-button--primary': primary,
-      'mdl-button--accent': accent,
-      'mdl-js-ripple-effect': rippleEffect
-    }, className);
-
-    return (
-      <button className={classes} {...otherProps}>
-        {children}
-      </button>
-    )
-  }
+  return (
+    <button className={classes} {...otherProps}>
+      {children}
+    </button>
+  )
 }
+
+Button.propTypes = {
+  className: PropTypes.string,
+  type: PropTypes.string,
+  raised: PropTypes.bool,
+  fab: PropTypes.bool,
+  miniFab: PropTypes.bool,
+  icon: PropTypes.bool,
+  colored: PropTypes.bool,
+  primary: PropTypes.bool,
+  accent: PropTypes.bool,
+  rippleEffect: PropTypes.bool
+}
+
+Button.defaultProps = {
+  type: 'submit',
+  raised: false,
+  fab: false,
+  miniFab: false,
+  icon: false,
+  colored: false,
+  primary: false,
+  accent: false,
+  rippleEffect: true
+}
+
+export default Button;
