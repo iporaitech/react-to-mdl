cdeea038c6f0af71aff7d77a4f7c55ee53e521d8
hisa <hisa@iporaitech.com>
2017-02-08 16:35:25 -0300

    Change Grid and Cell to be functional components. Refs #11

--- src/grid/Grid.js
+++ src/grid/Grid.js
@@ -1,25 +1,23 @@
-// file: shared/grid/Grid.js
-
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 
-export default class Grid extends React.Component {
-  static propTypes = {
-    className: PropTypes.string,
-    noSpacing: PropTypes.bool
-  }
+const Grid = (props) => {
+  const { className, noSpacing, children, ...otherProps } = props;
 
-  render() {
-    const { className, noSpacing, children, ...otherProps } = this.props;
+  const classes = classNames('mdl-grid', {
+    'mdl-grid--nospacing': noSpacing
+  }, className);
 
-    const classes = classNames('mdl-grid', {
-      'mdl-grid--nospacing': noSpacing
-    }, className);
-
-    return (
-      <div className={classes} {...otherProps}>
-        {children}
-      </div>
-    )
-  }
+  return (
+    <div className={classes} {...otherProps}>
+      {children}
+    </div>
+  )
 }
+
+Grid.propTypes = {
+  className: PropTypes.string,
+  noSpacing: PropTypes.bool
+}
+
+export default Grid;
--- src/grid/index.js
+++ src/grid/index.js
@@ -2,9 +2,9 @@
  * React components to render MDL grid components
  * See: https://github.com/google/material-design-lite/tree/v1.2.1/src/grid
  */
- 
+
 import Grid from './Grid';
 
 /*** exports ***/
 export default Grid;
-export {Cell} from './Cell.js';
+export { Cell } from './Cell.js';
