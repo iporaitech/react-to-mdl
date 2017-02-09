19927d1118da4771b173d6df9fca897d68f35081
hisa <hisa@iporaitech.com>
2017-02-09 15:43:59 -0300

    Convert Textfield class to functional component. Refs #11

--- src/textfield/index.js
+++ src/textfield/index.js
@@ -6,49 +6,41 @@
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 
-export default class Textfield extends React.Component {
-  static propTypes = {
-    id: PropTypes.string.isRequired,
-    className: PropTypes.string,
-    tag: PropTypes.oneOf(['input', 'textarea']),
-    labelText: PropTypes.string,
-    floatingLabel: PropTypes.bool,
-    hasError: PropTypes.bool
-  }
+const Textfield = (props) => {
+  const { id, className, tag, labelText, floatingLabel,
+    hasError, ...otherProps } = props;
 
-  static defaultProps = {
-    tag: 'input',
-    floatingLabel: true,
-    hasError: false
-  }
+  const classes = classNames('mdl-js-textfield mdl-textfield', {
+    'mdl-textfield--floating-label': floatingLabel,
+    'is-invalid': hasError
+  }, className);
 
- /**
-  * Return the value of the input
-  */
-  value() {
-    return this.refs.input.value;
-  }
-
-  render() {
-    const { id, className, tag, labelText, floatingLabel,
-      hasError, ...otherProps } = this.props;
-
-    const classes = classNames('mdl-js-textfield mdl-textfield', {
-      'mdl-textfield--floating-label': floatingLabel,
-      'is-invalid': hasError
-    }, className);
-
-    return (
-      <div className={classes} >
-        {React.createElement(
-          tag, Object.assign({}, otherProps, {
-            id: id,
-            ref: 'input',
-            className: 'mdl-textfield__input'
-          })
-        )}
-        <label htmlFor={id} className="mdl-textfield__label">{labelText}</label>
-      </div>
-    );
-  }
+  return (
+    <div className={classes} >
+      {React.createElement(
+        tag, Object.assign({}, otherProps, {
+          id: id,
+          className: 'mdl-textfield__input'
+        })
+      )}
+      <label htmlFor={id} className="mdl-textfield__label">{labelText}</label>
+    </div>
+  );
 }
+
+Textfield.propTypes = {
+  id: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  tag: PropTypes.oneOf(['input', 'textarea']),
+  labelText: PropTypes.string,
+  floatingLabel: PropTypes.bool,
+  hasError: PropTypes.bool
+}
+
+Textfield.defaultProps = {
+  tag: 'input',
+  floatingLabel: true,
+  hasError: false
+}
+
+export default Textfield;
