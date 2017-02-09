a1ed95522a5046da3f134c03bdececcb03ee3bca
hisa <hisa@iporaitech.com>
2017-02-09 15:16:50 -0300

    Convert example App to a functional component. Refs #11

--- src/examples/App.js
+++ src/examples/App.js
@@ -27,77 +27,76 @@ import Card, {
 // Styles for App
 import './app.scss';
 
-class App extends React.Component {
-  render() {
-    return (
-      <Layout>
-        <LayoutHeader>
-          <LayoutIcon />
-          <LayoutHeaderRow>
-            <LayoutTitle>react-to-mdl examples</LayoutTitle>
-            <LayoutSpacer />
-            <Navigation>
-              <NavigationLink to="/">Home</NavigationLink>
-            </Navigation>
-          </LayoutHeaderRow>
-        </LayoutHeader>
-
-        <LayoutDrawer>
+const App = (props) => {
+  return (
+    <Layout>
+      <LayoutHeader>
+        <LayoutIcon />
+        <LayoutHeaderRow>
+          <LayoutTitle>react-to-mdl examples</LayoutTitle>
+          <LayoutSpacer />
           <Navigation>
-            <NavigationLink href="/">Home</NavigationLink>
+            <NavigationLink to="/">Home</NavigationLink>
           </Navigation>
-        </LayoutDrawer>
+        </LayoutHeaderRow>
+      </LayoutHeader>
 
-        <LayoutContent>
-          <p className="grid-cell-description">
-            Examples are shown in a Cells inside Grids components
-          </p>
-          <Grid>
-            <Cell desktop={2} className="example-block">
-              <h6>
-                Spinner
-              </h6>
-              <Spinner />
-            </Cell>
+      <LayoutDrawer>
+        <Navigation>
+          <NavigationLink href="/">Home</NavigationLink>
+        </Navigation>
+      </LayoutDrawer>
 
-            <Cell desktop={2} className="example-block">
-              <h6>
-                Button
-              </h6>
-              <Button type='button' raised primary colored>
-                Push me
-              </Button>
-            </Cell>
+      <LayoutContent>
+        <p className="grid-cell-description">
+          Examples are shown in a Cells inside Grids components
+        </p>
+        <Grid>
+          <Cell desktop={2} className="example-block">
+            <h6>
+              Spinner
+            </h6>
+            <Spinner />
+          </Cell>
 
-            <Cell desktop={6} className="example-block">
-              <h6>
-                Card (Wide)
-              </h6>
-              <Card className="demo-card-wide">
-                <CardTitle>
-                  <CardTitleText tag='h2'>Welcome</CardTitleText>
-                </CardTitle>
-                <CardSupportingText>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis pellentesque lacus eleifend lacinia...
-                </CardSupportingText>
-                <CardActions border={true}>
-                  <Button tag='a' colored rippleEffect>
-                    Get Started
-                  </Button>
-                </CardActions>
-                <CardMenu>
-                  <Button icon rippleEffect>
-                    <i className="material-icons">share</i>
-                  </Button>
-                </CardMenu>
-              </Card>
-            </Cell>
+          <Cell desktop={2} className="example-block">
+            <h6>
+              Button
+            </h6>
+            <Button type='button' raised primary colored>
+              Push me
+            </Button>
+          </Cell>
 
-          </Grid>
-        </LayoutContent>
-      </Layout>
-    );
-  }
+          <Cell desktop={6} className="example-block">
+            <h6>
+              Card (Wide)
+            </h6>
+            <Card className="demo-card-wide">
+              <CardTitle>
+                <CardTitleText tag='h2'>Welcome</CardTitleText>
+              </CardTitle>
+              <CardSupportingText>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis pellentesque lacus eleifend lacinia...
+              </CardSupportingText>
+              <CardActions border={true}>
+                <Button tag='a' colored rippleEffect>
+                  Get Started
+                </Button>
+              </CardActions>
+              <CardMenu>
+                <Button icon rippleEffect>
+                  <i className="material-icons">share</i>
+                </Button>
+              </CardMenu>
+            </Card>
+          </Cell>
+
+        </Grid>
+      </LayoutContent>
+    </Layout>
+  );
+
 }
 
 /** exports **/
